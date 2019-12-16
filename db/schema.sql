5293d8634b4d3002b89894845034c28e278d0997
CJ Conrad <carlj.conrad@CJs-MacBook-Pro.local>
2019-12-16 12:34:28 -0800

    schema complete

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burgers_DB;
+CREATE database burgers_DB;
+
+USE burgers_DB;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(100) NOT NULL,
+  devoured BOOLEAN DEFAUTLT false,
+  PRIMARY KEY (id)
+);
+
+
+
