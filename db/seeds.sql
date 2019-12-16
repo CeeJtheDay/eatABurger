dde3281a4dfd2c615a01fb028a63b7bcc2f89e69
CJ Conrad <carlj.conrad@CJs-MacBook-Pro.local>
2019-12-16 12:42:23 -0800

    seeds file finished

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_DB;
+
+-- INSERT INTO people (name, has_pet, pet_name, pet_age)
+-- VALUES ("Ahmed", true, "Rockington", 100);
+INSERT INTO 
+    burgers (burger_name)
+VALUES 
+(Lemongrass Pork Burger),
+(Wild Boar Burger),
+(Club Burger);
