8300e2477200f3bc85b9808c957dbdf9efd53b15
CJ Conrad <carlj.conrad@CJs-MacBook-Pro.local>
2019-12-16 13:11:45 -0800

    orm layout finished

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+const connection = require("./connection.js");
+
+
+var orm = {
+    selectAll: function(callback) {
+
+    },
+
+    insertOne: function(callback) {
+
+    },
+
+    updateOne: function(callback) {
+
+    }
+}
+
+module.exports = orm;
